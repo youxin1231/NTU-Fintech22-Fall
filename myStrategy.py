09c3b3450199187dc4965d9b0b96b6355726c9de
youxin1231 <tommy135721@gmail.com>
2022-10-18 15:41:56 +0800

    Update Strategy parameters

--- myStrategy.py
+++ myStrategy.py
@@ -4,9 +4,9 @@ import talib
 def myStrategy(pastPriceVec, currentPrice):
 	# Explanation of my approach:
 	# 1. Technical indicator used: MACD
-	# 2. if price-ma>alpha ==> buy
-	#    if price-ma<-beta ==> sell
-	# 3. Modifiable parameters: alpha, beta, and window size for MA
+	# 2. if RSI > alpha ==> buy
+	#    if RSI < -beta ==> sell
+	# 3. Modifiable parameters: alpha, beta, and time period of MA
 	# 4. Use exhaustive search to obtain these parameter values (as shown in bestParamByExhaustiveSearch.py)
 	
 	# Set best parameters
@@ -14,22 +14,38 @@ def myStrategy(pastPriceVec, currentPrice):
 
 	PriceVec = np.append(pastPriceVec, currentPrice)
 	
-	timeperiod = 30
-	alpha = 65
-	beta = 25
+	long_period = 22
+	short_period = 7
+	alpha = 62
+	beta = 30
+	# The performance of Golden Cross / Death Cross
+	x = 1
+	y = 1
 
 	dataLen=len(PriceVec)	# Length of the data vector
-	if dataLen < timeperiod:
+	if dataLen < long_period:
 		action = 0
 	else:
 		# Compute long RSI
-		rsilist_long = talib.RSI(PriceVec[-(timeperiod+1):], timeperiod)
+		rsilist_long = talib.RSI(PriceVec[-(long_period+1):], long_period)
 		rsi_long = rsilist_long[-1]
 
-		# Determine actions based on long term RSI
+		# Compute short RSI
+		rsilist_short = talib.RSI(PriceVec[-(short_period+1):], short_period)
+		rsi_short = rsilist_short[-1]
+
+		# Determine action
+
+		# Based on long term RSI
 		if (rsi_long > alpha):
 			action = 1
 		elif (rsi_long < beta):
 			action = -1
 			
+		# Check if there's a Golden Cross / Death Cross
+		if (rsi_short - rsi_long > x):
+			action = 1
+		elif (rsi_short - rsi_long < -y):
+			action = -1
+
 	return action
